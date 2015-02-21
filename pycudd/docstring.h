449672265eed0ec3fc5b6d4f1c562b5c97af6b26
kariefury <carrie@skyberrys.com>
2015-02-21 00:01:58 +0000

    successful compile on Ubuntu 14.04 on GCC 64 bit microinstance

---
+++ pycudd/docstring.h
@@ -0,0 +1,1 @@
+#define DOCSTRING "PyCUDD 2.0.1\nPython interface to Colorado University Decision Diagram package\nWith BREL support\nCompiled on Feb 20 2015, 23:50:12\nBugs to:aravind@engr.ucsb.edu"
